d0ac847b270fef00b8aac7f09208cc3000249510
AceIsHuman <menoman1995@yahoo.com>
2019-09-24 22:11:05 -0500

    use connect to pass removeItem action as prop

--- src/App.js
+++ src/App.js
@@ -7,10 +7,6 @@ import AdditionalFeatures from './components/AdditionalFeatures';
 import Total from './components/Total';
 
 const App = props => {
-  const removeFeature = item => {
-    // dispatch an action here to remove an item
-  };
-
   return (
     <div className="boxes">
       <div className="box">
