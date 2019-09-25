7dc999c2b8503d03031c5ce32b00b9a48ee56dd6
AceIsHuman <menoman1995@yahoo.com>
2019-09-24 21:45:27 -0500

    move buyItem to AdditionalFeature component

--- src/App.js
+++ src/App.js
@@ -11,10 +11,6 @@ const App = props => {
     // dispatch an action here to remove an item
   };
 
-  const buyItem = item => {
-    // dipsatch an action here to add an item
-  };
-
   return (
     <div className="boxes">
       <div className="box">
